480a6643ab740d9c6c156d74e92f716090df58be
Kyle Grondin <kagron12@gmail.com>
2017-07-08 19:28:49 +0000

    Added flash message for url manipulation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :select_plan, only: :new
+  
   # Extend default devise gem behavior so that users signing up with the pro
   # account will save with a special Stripe subscription function
   def create
@@ -13,4 +15,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+  
+  private
+    def select_plan
+      unless (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up"
+        redirect_to root_path
+      end
+    end
 end
