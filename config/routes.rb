8af9f1f0eb6544f444461481bc2c9293e304803f
Kyle Grondin <kagron12@gmail.com>
2017-07-08 19:52:41 +0000

    Added user profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root to:'pages#home'
   devise_for :users, skip: [:sessions], controllers: {registrations: 'users/registrations'}
+  resources :users do
+    resource :profile
+  end
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new', as: :new_user_session
     post 'login', to: 'devise/sessions#create', as: :user_session
-    
     delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 end
