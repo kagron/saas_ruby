de3696f8b3e834d782cc858e3534b2fabb6059d5
Kyle Grondin <kagron12@gmail.com>
2017-07-13 15:24:31 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
   # User makes a GET request to /users/:user_id/profile/new
   def new
     # Render a blank profile form
-    
+    @profile = Profile.new
   end
 end
